4a772c757f18bedcdc132cd73fc327522718296d
Luca Imbalzano <lucaimbalzano@gmail.com>
2025-01-27 20:45:00 +0100

    added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -24,6 +25,7 @@ async function bootstrap() {
     },
   });
 
+  app.use(helmet());
   await app.listen(3000);
 }
 
