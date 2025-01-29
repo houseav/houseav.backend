4f12004b992b9b7d26ee6bba1f45580aaf82df24
Luca Imbalzano <lucaimbalzano@gmail.com>
2025-01-29 11:22:54 +0100

    updated ci

--- src/policy/policy.service.ts
+++ src/policy/policy.service.ts
@@ -1,10 +1,6 @@
-import { Injectable, UseGuards } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreatePolicyDto } from './dto/create-policy.dto';
 import { UpdatePolicyDto } from './dto/update-policy.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Public } from 'src/decorators/public.decorator';
 
 @Injectable()
 export class PolicyService {
